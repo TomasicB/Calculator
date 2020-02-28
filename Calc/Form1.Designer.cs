20746ae91836f4c3838d88cfb74b7618f0ccc419
TomasicB <60291195+TomasicB@users.noreply.github.com>
2020-02-28 13:27:13 +0100

    Add files via upload

---
+++ Calc/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿namespace Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bDecimal = new System.Windows.Forms.Button();
+            this.bDiv = new System.Windows.Forms.Button();
+            this.bMin = new System.Windows.Forms.Button();
+            this.bPlus = new System.Windows.Forms.Button();
+            this.bMul = new System.Windows.Forms.Button();
+            this.bDel = new System.Windows.Forms.Button();
+            this.bBackspace = new System.Windows.Forms.Button();
+            this.bEqual = new System.Windows.Forms.Button();
+            this.bSqrt = new System.Windows.Forms.Button();
+            this.bExp = new System.Windows.Forms.Button();
+            this.bTg = new System.Windows.Forms.Button();
+            this.bCos = new System.Windows.Forms.Button();
+            this.bSin = new System.Windows.Forms.Button();
+            this.tbScreen = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(11, 182);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(35, 35);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(52, 182);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(35, 35);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(93, 182);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(35, 35);
+            this.b3.TabIndex = 2;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(93, 141);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(35, 35);
+            this.b6.TabIndex = 5;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(52, 141);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(35, 35);
+            this.b5.TabIndex = 4;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(11, 141);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(35, 35);
+            this.b4.TabIndex = 3;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(93, 100);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(35, 35);
+            this.b9.TabIndex = 8;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(52, 100);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(35, 35);
+            this.b8.TabIndex = 7;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(11, 100);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(35, 35);
+            this.b7.TabIndex = 6;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b0
+            // 
+            this.b0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b0.Location = new System.Drawing.Point(11, 223);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(76, 35);
+            this.b0.TabIndex = 9;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // bDecimal
+            // 
+            this.bDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bDecimal.Location = new System.Drawing.Point(93, 223);
+            this.bDecimal.Name = "bDecimal";
+            this.bDecimal.Size = new System.Drawing.Size(35, 35);
+            this.bDecimal.TabIndex = 10;
+            this.bDecimal.Text = ",";
+            this.bDecimal.UseVisualStyleBackColor = true;
+            this.bDecimal.Click += new System.EventHandler(this.bDecimal_Click);
+            // 
+            // bDiv
+            // 
+            this.bDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bDiv.Location = new System.Drawing.Point(175, 182);
+            this.bDiv.Name = "bDiv";
+            this.bDiv.Size = new System.Drawing.Size(35, 35);
+            this.bDiv.TabIndex = 14;
+            this.bDiv.Text = "/";
+            this.bDiv.UseVisualStyleBackColor = true;
+            this.bDiv.Click += new System.EventHandler(this.bDiv_Click);
+            // 
+            // bMin
+            // 
+            this.bMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bMin.Location = new System.Drawing.Point(175, 141);
+            this.bMin.Name = "bMin";
+            this.bMin.Size = new System.Drawing.Size(35, 35);
+            this.bMin.TabIndex = 13;
+            this.bMin.Text = "-";
+            this.bMin.UseVisualStyleBackColor = true;
+            this.bMin.Click += new System.EventHandler(this.bMin_Click);
+            // 
+            // bPlus
+            // 
+            this.bPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bPlus.Location = new System.Drawing.Point(134, 141);
+            this.bPlus.Name = "bPlus";
+            this.bPlus.Size = new System.Drawing.Size(35, 35);
+            this.bPlus.TabIndex = 12;
+            this.bPlus.Text = "+";
+            this.bPlus.UseVisualStyleBackColor = true;
+            this.bPlus.Click += new System.EventHandler(this.bPlus_Click);
+            // 
+            // bMul
+            // 
+            this.bMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bMul.Location = new System.Drawing.Point(134, 182);
+            this.bMul.Name = "bMul";
+            this.bMul.Size = new System.Drawing.Size(35, 35);
+            this.bMul.TabIndex = 11;
+            this.bMul.Text = "*";
+            this.bMul.UseVisualStyleBackColor = true;
+            this.bMul.Click += new System.EventHandler(this.bMul_Click);
+            // 
+            // bDel
+            // 
+            this.bDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bDel.Location = new System.Drawing.Point(175, 99);
+            this.bDel.Name = "bDel";
+            this.bDel.Size = new System.Drawing.Size(35, 35);
+            this.bDel.TabIndex = 17;
+            this.bDel.Text = "C";
+            this.bDel.UseVisualStyleBackColor = true;
+            this.bDel.Click += new System.EventHandler(this.bDel_Click);
+            // 
+            // bBackspace
+            // 
+            this.bBackspace.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBackspace.Location = new System.Drawing.Point(134, 100);
+            this.bBackspace.Name = "bBackspace";
+            this.bBackspace.Size = new System.Drawing.Size(35, 35);
+            this.bBackspace.TabIndex = 16;
+            this.bBackspace.Text = "CE";
+            this.bBackspace.UseVisualStyleBackColor = true;
+            this.bBackspace.Click += new System.EventHandler(this.bBackspace_Click);
+            // 
+            // bEqual
+            // 
+            this.bEqual.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bEqual.Location = new System.Drawing.Point(134, 223);
+            this.bEqual.Name = "bEqual";
+            this.bEqual.Size = new System.Drawing.Size(76, 35);
+            this.bEqual.TabIndex = 15;
+            this.bEqual.Text = "=";
+            this.bEqual.UseVisualStyleBackColor = true;
+            this.bEqual.Click += new System.EventHandler(this.bEqual_Click);
+            // 
+            // bSqrt
+            // 
+            this.bSqrt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bSqrt.Location = new System.Drawing.Point(175, 59);
+            this.bSqrt.Name = "bSqrt";
+            this.bSqrt.Size = new System.Drawing.Size(35, 35);
+            this.bSqrt.TabIndex = 22;
+            this.bSqrt.Text = "sqr";
+            this.bSqrt.UseVisualStyleBackColor = true;
+            this.bSqrt.Click += new System.EventHandler(this.bSqrt_Click);
+            // 
+            // bExp
+            // 
+            this.bExp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bExp.Location = new System.Drawing.Point(134, 59);
+            this.bExp.Name = "bExp";
+            this.bExp.Size = new System.Drawing.Size(35, 35);
+            this.bExp.TabIndex = 21;
+            this.bExp.Text = "^";
+            this.bExp.UseVisualStyleBackColor = true;
+            this.bExp.Click += new System.EventHandler(this.bExp_Click);
+            // 
+            // bTg
+            // 
+            this.bTg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bTg.Location = new System.Drawing.Point(93, 59);
+            this.bTg.Name = "bTg";
+            this.bTg.Size = new System.Drawing.Size(35, 35);
+            this.bTg.TabIndex = 20;
+            this.bTg.Text = "tan";
+            this.bTg.UseVisualStyleBackColor = true;
+            this.bTg.Click += new System.EventHandler(this.bTg_Click);
+            // 
+            // bCos
+            // 
+            this.bCos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCos.Location = new System.Drawing.Point(52, 59);
+            this.bCos.Name = "bCos";
+            this.bCos.Size = new System.Drawing.Size(35, 35);
+            this.bCos.TabIndex = 19;
+            this.bCos.Text = "cos";
+            this.bCos.UseVisualStyleBackColor = true;
+            this.bCos.Click += new System.EventHandler(this.bCos_Click);
+            // 
+            // bSin
+            // 
+            this.bSin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bSin.Location = new System.Drawing.Point(11, 59);
+            this.bSin.Name = "bSin";
+            this.bSin.Size = new System.Drawing.Size(35, 35);
+            this.bSin.TabIndex = 18;
+            this.bSin.Text = "sin";
+            this.bSin.UseVisualStyleBackColor = true;
+            this.bSin.Click += new System.EventHandler(this.bSin_Click);
+            // 
+            // tbScreen
+            // 
+            this.tbScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbScreen.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tbScreen.Location = new System.Drawing.Point(11, 12);
+            this.tbScreen.Multiline = true;
+            this.tbScreen.Name = "tbScreen";
+            this.tbScreen.ReadOnly = true;
+            this.tbScreen.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tbScreen.Size = new System.Drawing.Size(199, 40);
+            this.tbScreen.TabIndex = 23;
+            this.tbScreen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 266);
+            this.Controls.Add(this.tbScreen);
+            this.Controls.Add(this.bSqrt);
+            this.Controls.Add(this.bExp);
+            this.Controls.Add(this.bTg);
+            this.Controls.Add(this.bCos);
+            this.Controls.Add(this.bSin);
+            this.Controls.Add(this.bDel);
+            this.Controls.Add(this.bBackspace);
+            this.Controls.Add(this.bEqual);
+            this.Controls.Add(this.bDiv);
+            this.Controls.Add(this.bMin);
+            this.Controls.Add(this.bPlus);
+            this.Controls.Add(this.bMul);
+            this.Controls.Add(this.bDecimal);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bDecimal;
+        private System.Windows.Forms.Button bDiv;
+        private System.Windows.Forms.Button bMin;
+        private System.Windows.Forms.Button bPlus;
+        private System.Windows.Forms.Button bMul;
+        private System.Windows.Forms.Button bDel;
+        private System.Windows.Forms.Button bBackspace;
+        private System.Windows.Forms.Button bEqual;
+        private System.Windows.Forms.Button bSqrt;
+        private System.Windows.Forms.Button bExp;
+        private System.Windows.Forms.Button bTg;
+        private System.Windows.Forms.Button bCos;
+        private System.Windows.Forms.Button bSin;
+        private System.Windows.Forms.TextBox tbScreen;
+    }
+}
+
